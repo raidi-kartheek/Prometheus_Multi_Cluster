e98a112f5ab214d8bea018435d809779aa7091ba
Your Name <you@example.com>
2026-03-16 14:18:50 +0530

    msg

---
+++ prometheus/Helm_repo.sh
@@ -0,0 +1,8 @@
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+
+helm install prometheus prometheus-community/prometheus \
+-n monitoring \
+--create-namespace \
+-f values.yaml
