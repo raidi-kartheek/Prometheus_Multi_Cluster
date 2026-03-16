a2cd556fd4be9e17626434a34245b01b8049f6c4
Your Name <you@example.com>
2026-03-16 14:43:52 +0530

    msg

--- prometheus/Helm_repo.sh
+++ prometheus/Helm_repo.sh
@@ -1,4 +1,5 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add prometheus-community https://github.com/raidi-kartheek/Prometheus_Multi_Cluster.git
+
 helm repo update
 
 
